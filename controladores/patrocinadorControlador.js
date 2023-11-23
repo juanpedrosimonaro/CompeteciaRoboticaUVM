042fa175f1a21b1f646cb4fcc63c4f4cdfe13ff0
Juan Simonaro <juansimonaro@emailnow.one>
2023-11-23 14:49:20 -0400

    Gestion patrocinadores

--- controladores/patrocinadorControlador.js
+++ controladores/patrocinadorControlador.js
@@ -1,16 +1,44 @@
 const seqSync = require('../modelos/asociador');
 
-const index = (req,res) => {
-  res.render('index', {patrocinadores:req.session.patrocinadores});
+const index = async (req,res) => {
+  const nombre = req.body.nombre;
+  try{
+    const { Equipo, Patrocinador } = await seqSync;
+    const patrocinadores = await Patrocinador.findAll();
+    res.render('gestionPatrocinadores', {patrocinadores,title:"Gestion Patrocinadores"});
+  }catch(error){
+    console.error(error);
+    res.status(300).json(error);
+  }
+}
+
+const mostrar = async (req,res) => {
+  const id = req.params.id;
+  try{
+    const { Equipo, Patrocinador } = await seqSync;
+    const equipos = await Equipo.findAll();
+    const patrocinador = await Patrocinador.findByPk(id,{include: Equipo});
+    res.render('gestionPatrocinador', {patrocinador,equipos,title:"Gestion de Patrocinador"});
+  }catch(error){
+    console.error(error);
+    res.status(300).json(error);
+  }
 }
 
 const create_post = async (req,res) => {
   const nombre = req.body.nombre;
-  const equipos = req.body.equipos || [];
   try{
     const { Equipo, Patrocinador } = await seqSync;
     const patrocinador = await Patrocinador.create({ nombre });
-    await patrocinador.setEquipos(equipos);
+    if(req.body.equipos){
+      const equipos = req.body.equipos;
+      await patrocinador.setEquipos(equipos);
+    }
+    // if(req.body.equipoId){
+    //   const equipoId = req.body.equipoId
+    //   const equipo = await Equipo.findByPk(equipoId);
+    //   await patrocinador.addEquipo(equipo)
+    // }
     res.status(200).json({patrocinador,equipos:(await patrocinador.getEquipos())});
   }catch(error){
     console.error(error);
@@ -45,10 +73,77 @@ const mostrarPorEquipo = async (req,res) =>{
   }
 }
 
+const editar = async (req,res) => {
+  const id = req.params.id;
+  const nombre = req.body.nombre;
+  try{
+    const { Patrocinador } = await seqSync;
+    const patrocinador = await Patrocinador.findByPk(id);
+    patrocinador.nombre = nombre;
+    await patrocinador.save();
+    res.status(200).json({patrocinador});
+  }
+  catch(error){
+    console.error(error);
+    res.status(300).json(error);
+  }
+}
+
+const eliminar = async (req,res) =>{
+  const id = Number(req.body.id);
+  try{
+    const { Patrocinador } = await seqSync;
+    const patrocinador = await Patrocinador.findByPk(id);
+    await patrocinador.destroy();
+    res.status(200).json({eliminado:true});
+  }
+  catch(error){
+    console.error(error);
+    res.status(300).json(error);
+  }
+}
+
+const patrocinarEquipo = async (req,res) =>{
+  const eqId = req.body.eqId;
+  const pId = req.body.pId;
+  try{
+    const { Patrocinador, Equipo } = await seqSync;
+    const equipo = await Equipo.findByPk(eqId);
+    const patrocinador = await Patrocinador.findByPk(pId);
+    await patrocinador.addEquipo(equipo);
+    res.status(200).json({patrocinador,equipo:(await patrocinador.getEquipos())})
+  }
+  catch(error){
+    console.error(error);
+    res.status(300).json(error);
+  }
+}
+
+const eliminarPatrocinioEquipo = async (req,res) =>{
+  const eqId = req.body.eqId;
+  const pId = req.body.pId;
+  try{
+    const { Patrocinador, Equipo } = await seqSync;
+    const equipo = await Equipo.findByPk(eqId);
+    const patrocinador = await Patrocinador.findByPk(pId);
+    await patrocinador.removeEquipo(equipo);
+    res.status(200).json({eliminado:true})
+  }
+  catch(error){
+    console.error(error);
+    res.status(300).json(error);
+  }
+}
+
 module.exports = {
   index,
   create_post,
   create_get,
   mostrarTodos,
-  mostrarPorEquipo
+  mostrarPorEquipo,
+  editar,
+  eliminar,
+  mostrar,
+  patrocinarEquipo,
+  eliminarPatrocinioEquipo
 } 
--- rutas/patrocinadorRutas.js
+++ rutas/patrocinadorRutas.js
@@ -5,9 +5,13 @@ const router = express.Router();
 
 router.get('/ingresar-patrocinador',patrocinadorControlador.create_get);
 router.get('/patrocinadores',patrocinadorControlador.mostrarTodos);
+router.get('/gestion-patrocinadores',patrocinadorControlador.index);
+router.get('/gestionar-patrocinador/:id',patrocinadorControlador.mostrar);
 router.post('/ingresar-patrocinador',patrocinadorControlador.create_post);
 router.get('/patrocinadoresPorEquipo',patrocinadorControlador.mostrarPorEquipo);
-// router.put('/editar-patrocinador/:id',patrocinadorControlador.editar);
-// router.delete('/eliminar-patrocinador/',patrocinadorControlador.eliminar);
+router.put('/editar-patrocinador/:id',patrocinadorControlador.editar);
+router.delete('/eliminar-patrocinador/',patrocinadorControlador.eliminar);
+router.post('/patrocinar-equipo',patrocinadorControlador.patrocinarEquipo);
+router.delete('/eliminar-patrocinio',patrocinadorControlador.eliminarPatrocinioEquipo)
 
 module.exports = router;
